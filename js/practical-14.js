c8a1dd86a7ab4989c842c5ba7130f7104348d017
mayank190821 <mayank.123.2000@gmail.com>
2021-10-14 08:46:26 +0530

    counter program

---
+++ js/practical-14.js
@@ -0,0 +1,36 @@
+var count=0;
+var valueChange = document.getElementById("counterValue");
+
+window.onload=()=>{
+    if(count ===0 ){
+        var bt= document.querySelector("button");
+        bt.disabled = true;
+    }
+    else{
+            var bt= document.querySelector("button");
+            bt.disabled = false;
+    }
+}
+
+function increment(){
+    if(count ===0 ){
+        var bt= document.querySelector("button");
+        bt.disabled = true;
+    }
+    else{
+            var bt= document.querySelector("button");
+            bt.disabled = false;
+    }
+    valueChange.innerHTML = count++;
+}
+function decrement(){
+    count--;
+    if(count ===0 ){
+        var bt= document.querySelector("button");
+        bt.disabled = true;
+    }else{
+            var bt= document.querySelector("button");
+            bt.disabled = false;
+    }
+    valueChange.innerHTML = count;
+}
